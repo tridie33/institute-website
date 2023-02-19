0852e148b43b328d197f83854391cf39a9b56e5d
jannounagnon@gmail.com <jannounagnon@gmail.com>
2023-02-19 13:46:24 +0100

    dropdown menu ready

---
+++ app/main.js
@@ -0,0 +1,27 @@
+
+
+// window.addEventListener('load', function () {
+//     alert("It's loaded!")
+//     const dropdown = document.querySelectorAll('.dropdown');
+
+
+//     for(var i = 1; i < 5; i++){
+//         dropdown[i].classList.add('bg');
+//         let enfants = dropdown[i].children;
+//         console.log(enfants)
+//         enfants[1].classList.add('hover-dropdown');
+//     }
+
+// })
+
+
+
+// while(1){
+    // for(var i = 0; i < 5; i++){
+    //     dropdown[i].addEventListener('hover', () =>{
+    //         let element = dropdown[i].querySelector('dropdown-item');
+    //         element.classList.add('hover-dropdown');
+    //     })
+    // } 
+// }
+
